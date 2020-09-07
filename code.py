cd43eb347052ca5bcb2121aee943f978b14ca0c0
Nayemur Rahaman Durjoy <38357909+NAYEMUR@users.noreply.github.com>
2020-09-08 01:35:44 +0600

    side note added

--- code.py
+++ code.py
@@ -4,26 +4,25 @@ import time
 pyautogui.FAILSAFE = False
 
 for x in range(0, 2):
-    time.sleep(5)
+    time.sleep(5) #wait for 5 second
     #for like
-    pyautogui.press('j')
+    pyautogui.press('j') #read a post
     time.sleep(2)
-    pyautogui.press('l')
+    pyautogui.press('l') #react as like
     time.sleep(2)
     pyautogui.press('enter')
     time.sleep(2)
     #for comment
-    pyautogui.press('c')
+    pyautogui.press('c') #c for comment
     time.sleep(2)
-    pyautogui.typewrite("this is comment",0.25)
+    pyautogui.typewrite("this is comment",0.25)  #write a comment
     time.sleep(2)
     pyautogui.press('enter')
     time.sleep(2)
-    pyautogui.press('tab')
+    pyautogui.press('tab') #leave from comment box
     time.sleep(2)
     #for share
-    pyautogui.press('s')
+    pyautogui.press('s') #s for share
     time.sleep(2)
     pyautogui.press('enter')
     time.sleep(2)
-
